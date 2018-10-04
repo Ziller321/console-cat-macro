52bbbccb942d221a075e8301784784b8881f31fe
Joonas Haaparanta <joonashaaparanta@gmail.com>
2018-10-04 15:25:34 +0300

    Cat

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # console-cat-macro
+
 babel macro for cats
+
+Will transform 80+ different cat strings to cat emoji in console.log
+
+usage
+import console from 'console-cat.macro'
+
+console.log("this is cat")
---
+++ index.js
@@ -0,0 +1,17 @@
+const {createMacro}  = require('babel-plugin-macros')
+module.exports = createMacro(emojiLog)
+function emojiLog({ references, state, babel }) {
+  references.default.forEach(referencePath => {
+    const [catPath] = referencePath.parentPath.parentPath.get('arguments')
+
+    const s = catPath.node.value;
+    // https://www.cat-world.com.au/cat-and-kitten-different-languages.html
+    var regex = / Cat | Kat | Mace | Dimeti | Qut | Pişik | Biṛāla | Katu | Kot | Mačka | Kotka | Kwayar ngya | Gat | Iring | Māo | Māo | Gattu |  Macka | Kočka | Kat | Kat | Kato | Kass | Pusa | Kissa | Chat | Gato | Katze | Yata | Bilāḍī | Chat | Cat | Anu | Billee | Miv | Macska | Pusi |  Köttur | Kuching | Gato | Neko | Kuching | Bekku | Mısıq | Chhma | Goyang-i | Pisîk | Mışık | Cat | Cattus | Kakis | Katė | Kaz | Mačka |  Kucing | Pūcca | Qattus | Māo | Ngeru | Mān̄jara | Muur |  Katt | Kot | Gato | Pisica | Kowka | Mayka | Baḷalā | Macka | Macka | Gato | Ucing | Paka | Katt | Koş | Pūṉai | Pilli | Mæw | Kedi | Kot | Mushuk | Meo | Cath | Inkonti | Kats | O nran | Ikati /gi;
+    const catString = s.replace(regex, '🐱');
+
+    const catNode = babel.types.stringLiteral(catString)
+    catPath.replaceWith(catNode)
+
+ })
+
+}

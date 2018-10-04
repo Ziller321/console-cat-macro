db020bb46b836d9b32c6523bd6296a5ba8f65add
Joonas Haaparanta <joonashaaparanta@gmail.com>
2018-10-04 15:43:03 +0300

    Better regex

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function emojiLog({ references, state, babel }) {
 
     const s = catPath.node.value;
     // https://www.cat-world.com.au/cat-and-kitten-different-languages.html
-    var regex = / Cat | Kat | Mace | Dimeti | Qut | Pişik | Biṛāla | Katu | Kot | Mačka | Kotka | Kwayar ngya | Gat | Iring | Māo | Māo | Gattu |  Macka | Kočka | Kat | Kat | Kato | Kass | Pusa | Kissa | Chat | Gato | Katze | Yata | Bilāḍī | Chat | Cat | Anu | Billee | Miv | Macska | Pusi |  Köttur | Kuching | Gato | Neko | Kuching | Bekku | Mısıq | Chhma | Goyang-i | Pisîk | Mışık | Cat | Cattus | Kakis | Katė | Kaz | Mačka |  Kucing | Pūcca | Qattus | Māo | Ngeru | Mān̄jara | Muur |  Katt | Kot | Gato | Pisica | Kowka | Mayka | Baḷalā | Macka | Macka | Gato | Ucing | Paka | Katt | Koş | Pūṉai | Pilli | Mæw | Kedi | Kot | Mushuk | Meo | Cath | Inkonti | Kats | O nran | Ikati /gi;
+    const regex = /\b(Cat|Kat|Mace|Dimeti|Qut|Pişik|Biṛāla|Katu|Kot|Mačka|Kotka|Kwayar ngya|Gat|Iring|Māo|Māo|Gattu| Macka|Kočka|Kat|Kat|Kato|Kass|Pusa|Kissa|Chat|Gato|Katze|Yata|Bilāḍī|Chat|Cat|Anu|Billee|Miv|Macska|Pusi| Köttur|Kuching|Gato|Neko|Kuching|Bekku|Mısıq|Chhma|Goyang-i|Pisîk|Mışık|Cat|Cattus|Kakis|Katė|Kaz|Mačka| Kucing|Pūcca|Qattus|Māo|Ngeru|Mān̄jara|Muur| Katt|Kot|Gato|Pisica|Kowka|Mayka|Baḷalā|Macka|Macka|Gato|Ucing|Paka|Katt|Koş|Pūṉai|Pilli|Mæw|Kedi|Kot|Mushuk|Meo|Cath|Inkonti|Kats|O nran|Ikati)\b/gi;
     const catString = s.replace(regex, '🐱');
 
     const catNode = babel.types.stringLiteral(catString)
